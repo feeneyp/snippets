24ce830dd9f9b48148f0a56ec336157a58013769
feeneyp <patrick@r3dpixel.com>
2015-02-12 12:21:45 +0000

    add parser structure

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -23,3 +25,35 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return "No such with name: {}.".format(name)  
+  
+  
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+
+
+    
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Store a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+
+    arguments = parser.parse_args(sys.argv[1:])
+    print(arguments)
+    
+
+if __name__ == "__main__":
+    main() 
+    
+    
+    
+    
+    
